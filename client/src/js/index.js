552c543e307783260ecb0d3e77c47c698e4b773f
mackgabe <mackenzieabe@gmail.com>
2022-09-03 13:50:11 -0400

    added render functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,9 +3,10 @@
 //Webpack works by creating a dependency graph of all the modules and assets using the entry point as the starting point.
 
 //Import modules 
-import  "./form";
-import "./submit";
-import { initDb, getDb, postDb } from './database'; 
+import "./form";
+import { initDb, getDb, postDb } from './database';
+import { fetchCards } from "./cards";
+import { toggleForm, clearForm} from "./form";
 
 //Import CSS files
 import "../css/index.css";
@@ -22,16 +23,47 @@ import Dog from '../images/dog.png';
 import { initdb } from './database';
 
 window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-    initDb()
-
-  // We are temporarily placing getDb() and postDb() function calls here for testing. We will move it to another event listener later.
-  getDb();
-  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
-  getDb();
+  initDb()
+  fetchCards();
+  //we added the fetchCards() function into our on-load event listener so that data is pulled from our IndexedDB database and a new card is created and rendered per data object. If you still have the getDb() and postDb() functions here, please remove them now.
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
 });
-   
-    
-  
+
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+});
+
